d4a7477a3b57d5fb849b882e34ebb046fd5e2152
RenanSantos023 <renan.santos023@outlook.com>
2026-08-11 12:28:30 -0300

    ajuste

---
+++ script.js
@@ -0,0 +1,59 @@
+const API =
+"https://SUA_API.execute-api.sa-east-1.amazonaws.com/prod/upload";
+
+async function upload(){
+
+const file =
+document.getElementById("video").files[0];
+
+if(!file){
+
+alert("Selecione um vídeo");
+
+return;
+
+}
+
+document.getElementById("status").innerHTML =
+"Obtendo URL...";
+
+const response =
+await fetch(API,{
+
+method:"POST",
+
+headers:{
+"Content-Type":"application/json"
+},
+
+body:JSON.stringify({
+
+fileName:file.name,
+
+contentType:file.type
+
+})
+
+});
+
+const data =
+await response.json();
+
+document.getElementById("status").innerHTML =
+"Enviando vídeo...";
+
+await fetch(data.uploadUrl,{
+
+method:"PUT",
+
+headers:{
+"Content-Type":file.type
+},
+
+body:file
+
+});
+
+document.getElementById("status").innerHTML =
+"Upload concluído!";
+}
